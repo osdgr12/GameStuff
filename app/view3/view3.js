778c1b29f3519a00661eec9d5169d057104a32c0
Gaurav <gauravshahhere@gmail.com>
2015-11-18 16:57:36 -0500

    Changes for initial dependency solving

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -1,3 +1,14 @@
-/**
- * Created by Student on 11/18/2015.
- */
+'use strict';
+
+angular.module('myApp.view3', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view3', {
+            templateUrl: 'view3/view3.html',
+            controller: 'View3Ctrl'
+        });
+    }])
+
+    .controller('View3Ctrl', [function() {
+        console.log('view3 is reached');
+    }]);

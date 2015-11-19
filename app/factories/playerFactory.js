c9e227524cfeab4a01afe02efa6713679cc663bf
Gaurav <gauravshahhere@gmail.com>
2015-11-19 11:47:41 -0500

    changes to player factory

--- app/factories/playerFactory.js
+++ app/factories/playerFactory.js
@@ -1,16 +1,23 @@
 /**
  * Created by Student on 11/18/2015.
  */
-angular.module('myApp').factory('playerFactory', function () {
+
+(function(){
     var players = [
         { id: 1, name: "Gaurav", image: "a.png", bb: 10, gb: 10},
-        { id: 2, name: "Rajesh", image: "b.png", bb: 10, gb: 10},
-        { id: 3, name: "Jason", image: "c.png", bb: 10, gb: 10},
-        { id: 4, name: "Mihaly", image: "d.png", bb: 10, gb: 10}
+        { id: 2, name: "Rajesh", image: "b.png", bb: 7, gb: 7},
+        { id: 3, name: "Jason", image: "c.png", bb: 4, gb: 5},
+        { id: 4, name: "Mihaly", image: "d.png", bb: 3, gb: 4}
     ];
-    var factory = {};
-    factory.getPlayers = function () {
-        return players;
+    var playerFactory=function(){
+        var factory = {};
+        factory.getPlayers = function () {
+            return players;
+        };
+        return factory;
     };
-    return factory;
-});
+
+    angular.module('myApp.view3').factory('view3Factory',playerFactory);
+
+}());
+

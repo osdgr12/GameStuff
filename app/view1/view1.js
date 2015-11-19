8e7398e5bda8a7a9145fdd32ab0447b7fc11f0b8
osdgr12 <osdgr12@gmail.com>
2015-11-19 13:30:10 -0500

    View1 Controlleer

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,18 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope','view1Factory',function($scope,view1Factory) {
+    console.log('view1 is reached');
+    if ($scope) {
+        console.log('Scope found in view1');
+        $scope.count = 10;
+    }
 
+    if (view1Factory) {
+        console.log('view3 factory exists');
+    }
+    $scope.myFunction = function () {
+        console.log('reached v1 event handler');
+        
+    };
 }]);
